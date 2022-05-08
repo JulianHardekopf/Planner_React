3b350785f3f2bbbd89bedeec2cf4670cff556ee5
JulianHardekopf <julian.hardekopf@stud.th-luebeck.de>
2022-05-09 00:11:19 +0200

    #10 Updated Eventmodal, working on the submit button

--- src/components/EventModal.js
+++ src/components/EventModal.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import {AiOutlineCloseCircle} from 'react-icons/ai'
 import  { useState } from 'react'
+import Select from 'react-select'
 
 
 const EventModal = ({openform,setopenform}) => {
     const [title, setTitle] = useState('')
+ 
+
+
+
+
   return (
     <div className='h-screen w-full fixed left-0 top-0 flex justify-start items-center' >
         <form className='bg-white rounded-lg shadow-2xl w-1/4'>
@@ -21,11 +27,12 @@ const EventModal = ({openform,setopenform}) => {
                     <div className= 'text-gray-600 text-xl font-semibold pb-2 w-full'>name</div>
                     <input className='col-span-2 pt-3 border-0 text-gray-600 text-xl font-semibold pb-2 w-full border-b-2 border-gray-200 focus:outline-none focus:ring-0 focus:border-indigo-900	' type="text" name='title' placeholder='Add title' value={title} required onChange={(e) => setTitle(e.target.value)} />
                     <div className='text-gray-600 text-xl font-semibold pb-2 w-full col-span-3'>
-                        <select name='day' id="day" className='w-full'>
+                        <select onClick={() => console.log("test")} name='day' id="day" className='w-full'>
                             <option value=''>Monday</option>
-                            <option value='second'>Monday</option>
+                            <option value='second'>Tuesday</option>
 
                         </select>
+                  
                     </div>
                    
                 </div>
